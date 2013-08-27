28cd03ebce9be3bb6786a0369ce9fce895b9c3b9
Patrick Mulvany <pm335@kent.ac.uk>
2013-08-27 10:31:13 +0100

    version fix for mod/aspirelists

--- version.php
+++ version.php
@@ -11,7 +11,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2012071004;
+$module->version   = 2012071005;
 $module->requires  = 2011112900;
 $module->component = 'mod_aspirelists'; 
 $module->cron      = 0;
