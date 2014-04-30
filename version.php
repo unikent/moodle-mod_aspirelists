d11b1f7f3917f970af5fd14a41db97669700ba59
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-04-30 08:47:06 +0100

    Version Bump

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014042300;
+$plugin->version   = 2014043000;
 $plugin->requires  = 2013110500;
 $plugin->component = 'mod_aspirelists';
 $plugin->cron      = 0;
